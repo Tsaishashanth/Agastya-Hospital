58b494fdf546cd1b026a689f369a7b6a00cc398e
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-06-25 17:45:04 +0530

    modified nav bars

--- agastya-hospital/src/App.js
+++ agastya-hospital/src/App.js
@@ -6,16 +6,24 @@ function App() {
 
     <>
     <div style={{ textAlign: 'center', maxWidth: '100%',margin: '0 auto',display: 'flex',justifyContent: 'center',
-alignItems: 'center', height: '13rem' }}>
-      <div className="leftbar"></div>
+alignItems: 'center' }}>
+      <div className="leftbar">
+        <a href= "#" className="nav-item">Home</a>
+        <a href= "#" className="nav-item">About Us</a>
+        <a href= "#" className="nav-item">Services</a>
+      </div>
 
       <img
         src="/agastya hospital image.png"
         alt="Agastya Hospital Logo"
         style={{ width: '170px', height: 'auto', borderRadius: '50%' }}
       />
-      
-      <div className= "rightbar"></div>
+
+      <div className= "rightbar">
+        <a href = "#"className ="nav-item">Appointment</a>
+        <a href = "#"className ="nav-item">Contact Us</a>
+        <a href = "#"className ="nav-item" style={{color:'red', fontWeight: 'bold'}}>Emergency</a>
+      </div> 
     </div>
     </>
   );
