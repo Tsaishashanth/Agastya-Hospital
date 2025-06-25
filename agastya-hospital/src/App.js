26bde68c0b5f45015b972e5abde3fb3f56cfb17a
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-06-25 05:41:35 +0530

    modified css

--- agastya-hospital/src/App.js
+++ agastya-hospital/src/App.js
@@ -1,37 +1,19 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    // just styling
 
     <>
     <div style={{ textAlign: 'center', maxWidth: '100%',margin: '0 auto',display: 'flex',justifyContent: 'center',
 alignItems: 'center', height: '13rem' }}>
-      <div style = {{
-      width:'100%',
-      height: '3rem',
-      backgroundColor: 'gray',
-      borderTopRightRadius: '2px',
-      borderBottomRightRadius: '2px',
-      borderRadius: '0%',
-      border :'4px solid gray',
-      zIndex:2
-    }}></div>
+      <div className="mainbar"></div>
       <img
         src="/agastya hospital image.png"
         alt="Agastya Hospital Logo"
         style={{ width: '170px', height: 'auto', borderRadius: '50%' }}
       />
-      <div style = {{
-      width:'100%',
-      height: '3rem',
-      backgroundColor: 'gray',
-      borderTopRightRadius: '2px',
-      borderBottomRightRadius: '2px',
-      borderRadius: '0%',
-      border :'4px solid gray',
-      zIndex:2
-    }}></div>
+      <div className= "mainbar"></div>
     </div>
     </>
   );
